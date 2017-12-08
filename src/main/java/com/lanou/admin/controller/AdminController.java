43220a7d92810994a6bfdad3408028f9c26e5e4d
zrcxing1128 <741187118@qq.com>
2017-12-08 10:37:05 +0800

    作者:张瑞超 时间:2017年12月08日10:23:46 第一次合作提交

--- src/main/java/com/lanou/admin/controller/AdminController.java
+++ src/main/java/com/lanou/admin/controller/AdminController.java
@@ -20,7 +20,7 @@ public class AdminController {
     @Qualifier("adminService")
     @Autowired
     private AdminService adminService;
-    //12312421312334535123234234
+    //123124213123345351232342346786
     @RequestMapping(value = "/goAddAdminPage")
     public String admin_Add(){
         return "admin/admin-add";
